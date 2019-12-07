5eb1581f8b185e78ec35b012e808e48dc331fc9d
Anwesh43 <anweshthecool0@gmail.com>
2019-12-07 15:06:31 +0530

    adding CornerBouncyBallView class

--- cornerbouncyballview/src/main/java/com/anwesh/uiprojects/cornerbouncyballview/CornerBouncyBallView.kt
+++ cornerbouncyballview/src/main/java/com/anwesh/uiprojects/cornerbouncyballview/CornerBouncyBallView.kt
@@ -60,3 +60,22 @@ fun Canvas.drawCBBNode(i : Int, scale : Float, paint : Paint) {
     drawCornerBouncyBalls(scale, size, paint)
     restore()
 }
+
+
+class CornerBouncyBallView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
