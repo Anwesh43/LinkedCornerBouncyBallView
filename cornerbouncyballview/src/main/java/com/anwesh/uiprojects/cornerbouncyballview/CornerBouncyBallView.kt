ceea0bb846d21798dc920250a22e53ba7ba56567
Anwesh43 <anweshthecool0@gmail.com>
2019-12-07 14:56:30 +0530

    adding constants and libraries

--- cornerbouncyballview/src/main/java/com/anwesh/uiprojects/cornerbouncyballview/CornerBouncyBallView.kt
+++ cornerbouncyballview/src/main/java/com/anwesh/uiprojects/cornerbouncyballview/CornerBouncyBallView.kt
@@ -4,3 +4,19 @@ package com.anwesh.uiprojects.cornerbouncyballview
  * Created by anweshmishra on 07/12/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val balls : Int = 4
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#009688")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rFactor : Float = 3.2f
