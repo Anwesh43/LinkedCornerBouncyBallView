85dde17e2b68b3c8d28da851a9a6a03854b22fd9
Anwesh43 <anweshthecool0@gmail.com>
2019-12-07 14:58:47 +0530

    adding float and int extension  methods

--- cornerbouncyballview/src/main/java/com/anwesh/uiprojects/cornerbouncyballview/CornerBouncyBallView.kt
+++ cornerbouncyballview/src/main/java/com/anwesh/uiprojects/cornerbouncyballview/CornerBouncyBallView.kt
@@ -20,3 +20,9 @@ val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#009688")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rFactor : Float = 3.2f
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+fun Float.cosify() : Float = 1f - Math.sin(Math.PI / 2 + (this) * Math.PI / 2).toFloat()
