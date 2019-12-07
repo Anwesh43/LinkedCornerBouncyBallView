3e6c3085dcafa1c9bd927df89b34708cb4883840
Anwesh43 <anweshthecool0@gmail.com>
2019-12-07 15:37:59 +0530

    adding fixes

--- cornerbouncyballview/src/main/java/com/anwesh/uiprojects/cornerbouncyballview/CornerBouncyBallView.kt
+++ cornerbouncyballview/src/main/java/com/anwesh/uiprojects/cornerbouncyballview/CornerBouncyBallView.kt
@@ -26,7 +26,7 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
-fun Float.cosify() : Float = 1f - Math.sin(Math.PI / 2 + (this) * Math.PI / 2).toFloat()
+fun Float.cosify() : Float = Math.sin(Math.PI / 2 + (this) * Math.PI / 2).toFloat()
 
 fun Canvas.drawCornerBouncyBall(i : Int, scale : Float, size : Float, paint : Paint) {
     val r : Float = size / rFactor
@@ -41,10 +41,7 @@ fun Canvas.drawCornerBouncyBall(i : Int, scale : Float, size : Float, paint : Pa
 
 fun Canvas.drawCornerBouncyBalls(scale : Float, size : Float, paint : Paint) {
     for (j in 0..(balls - 1)) {
-        save()
-        rotate(90f * j)
-        drawCornerBouncyBall(j, scale, size, paint)
-        restore()
+        drawCornerBouncyBall(j, scale.divideScale(j, balls), size, paint)
     }
 }
 
