f94b525dedc0ba2956c9397b33eeec121c41434a
Anwesh43 <anweshthecool0@gmail.com>
2019-12-07 15:25:57 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedcornerbouncyballview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcornerbouncyballview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedcornerbouncyballview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.cornerbouncyballview.CornerBouncyBallView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CornerBouncyBallView.create(this)
     }
 }
